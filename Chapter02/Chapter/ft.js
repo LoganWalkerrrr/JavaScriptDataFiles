2a3c6096541b98243d0164587a462c257aa1363f
Walker <logan.walker@stu.lee.kyschools.us>
2022-02-02 13:23:01 -0500

    In Class Work

--- Chapter02/Chapter/ft.js
+++ Chapter02/Chapter/ft.js
@@ -4,9 +4,20 @@
 
  *    Fan Trick Fine Art Photography
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Logan Walker
+ *    Date: 02/02/22
 
  *    Filename: ft.js
  */
+	//sets all form field vaules to default
+	function resetForm(){
+		document.getElementById("photognum").value = 1;
+		document.getElementById("photoghrs").value = 2;
+		document.getElementById("membook").checked = false;
+		document.getElementById("reprodrights").checked = false;
+		document.getElementById("distance").value = 0;
 
+}
+
+
+	
